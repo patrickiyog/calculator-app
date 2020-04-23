b33b498635d0be9f79277c61f744d77feedb27b6
patrickiyog <patrickiyog97@gmail.com>
2020-04-23 17:46:07 +1200

    Created Button and Keypad Components Files

---
+++ src/ButtonComponent/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Button.module.css';
+
+const Button = () => {
+  return (
+    <div className={classes.Button}>
+      <button>7</button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/KeypadComponent/Keypad.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '../ButtonComponent/Button';
+
+const Keypad = () => {
+  return <div></div>;
+};
+
+export default Keypad;
